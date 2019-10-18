a98c44375f9529de60a1d95fc54a75945109f0a1
Fessan <fessan@ya.ru>
2019-10-18 12:48:46 +0700

    step 3

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+export const greeting = console.log(`Welcome to the Brain Games!`);
+export const actual = readlineSync.question('May I have your name?');
+export const answer = console.log(`Hello, ${actual}!`);
